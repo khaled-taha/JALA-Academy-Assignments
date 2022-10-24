741cbc727aa6469e7d25be92729faa78a1a200f4
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-24 13:06:53 +0200

    Create PrintName.java

---
+++ 1. Programming Language Assignments./1. Java Basics/PrintName.java
@@ -0,0 +1,11 @@
+public class PrintName {
+
+    public static void main(String[] args) {
+        printName("Khaled Taha");
+    }
+
+    public static void printName(String name){
+        System.out.println(name);
+    }
+
+}
