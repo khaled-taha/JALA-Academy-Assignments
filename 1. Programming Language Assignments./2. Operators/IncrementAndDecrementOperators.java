cfa1d17bc53bd2deb0e126d223e8a50e4cd7f37a
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-24 14:30:15 +0200

    Create IncrementAndDecrementOperators.java

---
+++ 1. Programming Language Assignments./2. Operators/IncrementAndDecrementOperators.java
@@ -0,0 +1,11 @@
+public class IncrementAndDecrementOperators {
+  
+    public int increase(int num){
+        return num++;
+    }
+
+    public int decrease(int num){
+        return num--;
+    }
+  
+}
