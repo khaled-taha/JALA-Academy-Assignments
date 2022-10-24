36ff6e78a0937c24d57ff1ac710a4ffc312e10e4
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-24 14:28:02 +0200

    Update ArithmeticOperators.java

--- 1. Programming Language Assignments./2. Operators/ArithmeticOperators.java
+++ 1. Programming Language Assignments./2. Operators/ArithmeticOperators.java
@@ -1,18 +1,18 @@
 public class ArithmeticOperators {
   
-    public static int addition(int num1, int num2){
+    public static int add(int num1, int num2){
         return num1 + num2;
     }
 
-    public static int subtraction(int num1, int num2){
+    public static int subtract(int num1, int num2){
         return num1 - num2;
     }
 
-    public static int division(int num1, int num2) throws ArithmeticException{
+    public static int divide(int num1, int num2) throws ArithmeticException{
         return num1 / num2;
     }
 
-    public static int multiplication(int num1, int num2){
+    public static int multiply(int num1, int num2){
         return num1 * num2;
     }
   
