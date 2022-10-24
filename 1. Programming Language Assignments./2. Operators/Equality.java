62d2b4ccf2320d6c46c939b58831340caa26f609
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-24 14:34:48 +0200

    Create Equality.java

---
+++ 1. Programming Language Assignments./2. Operators/Equality.java
@@ -0,0 +1,10 @@
+public class Equality {
+
+    public boolean checkEquality(int num1, int num2){
+        return num1 == num2;
+    }
+
+    public boolean checkNotEquality(int num1, int num2){
+        return num1 != num2;
+    }
+}
