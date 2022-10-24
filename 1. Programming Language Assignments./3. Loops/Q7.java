49b339e2d79d37fff46c18603fed48783b664261
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-24 17:09:53 +0200

    Create Q7.java

---
+++ 1. Programming Language Assignments./3. Loops/Q7.java
@@ -0,0 +1,14 @@
+
+// 7. Write a program to print 1 to 10 using the do-while loop statement
+
+public class Q7 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        } while(i < 11);
+
+    }
+}
