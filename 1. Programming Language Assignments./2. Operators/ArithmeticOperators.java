ae2e34b643e96e5fb84e41eeee8e06462fe5a132
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-24 14:26:10 +0200

    Create ArithmeticOperators.java

---
+++ 1. Programming Language Assignments./2. Operators/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+public class ArithmeticOperators {
+  
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static int division(int num1, int num2) throws ArithmeticException{
+        return num1 / num2;
+    }
+
+    public static int multiplication(int num1, int num2){
+        return num1 * num2;
+    }
+  
+}
