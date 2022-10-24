0eb054cd2f60157f49a8ec1ac176ece6a890224f
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-24 16:39:40 +0200

    Create Q1.java

---
+++ 1. Programming Language Assignments./3. Loops/Q1.java
@@ -0,0 +1,12 @@
+// 1. Write a program to print “Bright IT Career” ten times using for loop.
+  
+public class Q1 {
+
+    public static void main(String[] args) {
+        
+        for(int i = 0; i < 10; i++){
+            System.out.println("Bright IT Career");
+        }
+
+    }
+}  
