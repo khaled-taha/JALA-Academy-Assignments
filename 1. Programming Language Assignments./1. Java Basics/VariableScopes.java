2b03b4aa2d354178553752f02e188f57ab5457d8
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-10-30 17:14:03 +0200

    Delete

--- 1. Programming Language Assignments./1. Java Basics/VariableScopes.java
+++
@@ -1,8 +0,0 @@
-public class VariableScopes {
-
-    int id = 1; // global variable
-
-    public void setId(int id /*local variable*/) {
-        this.id = id;
-    }
-}
--- 1. Programming Language Assignments./2. Operators/Equality2.java
+++
@@ -1,7 +0,0 @@
-public class Equality2 {
-
-    public boolean checkEquality(int num1, int num2){
-        return num1 == num2;
-    }
-  
-}
